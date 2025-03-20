3664247ae0185b3efc621441a74b1802f67555d7
tct123 <42028373+tct123@users.noreply.github.com>
2025-03-20 04:18:01 +0100

    .

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # MyMovieDB
  Just a Project to create a Database for discs like DVD or Bluray
-343a40
--- app.py
+++ app.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def mainpage():
-    return render_template("index.html")
+    return render_template("index.html", title="MyMovieDB")
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
